cdbf805f3fa82475623a83a4d6fa7b8bd56a2e9f
duffigoogle <calebdduff@gmail.com>
2022-07-19 21:07:45 +0000

    Add project files for Task 3 - Quick sort

--- sort.h
+++ sort.h
@@ -36,4 +36,7 @@ void insertion_sort_list(listint_t **list);
 /*Function that sorts an array of integers in ascending order using*/
 void selection_sort(int *array, size_t size);
 
+/*Function that sorts an array of integers in ascending order using*/
+void quick_sort(int *array, size_t size);
+
 #endif /* !_SORT_H_*/
